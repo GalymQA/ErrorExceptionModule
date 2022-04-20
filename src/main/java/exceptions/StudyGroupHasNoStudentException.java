680e79a3a8b4baa2ce8f9548d216b341546992eb
GalymQA <galymakishevkz@gmail.com>
2022-04-20 17:07:20 +0600

    Added Study Group Has No Student Exception

---
+++ src/main/java/exceptions/StudyGroupHasNoStudentException.java
@@ -0,0 +1,7 @@
+package main.java.exceptions;
+
+public class StudyGroupHasNoStudentException extends Exception {
+    public StudyGroupHasNoStudentException(String str) {
+        super(str);
+    }
+}
