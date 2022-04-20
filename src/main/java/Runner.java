3af95acccbdad584c43f5866231bc698319332e3
GalymQA <galymakishevkz@gmail.com>
2022-04-20 14:47:26 +0600

    Added fields to University class

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,7 +3,10 @@ package main.java;
 public class Runner {
 
     public static void main(String[] args) {
-                
+        Student student1 = new Student("Ivan","Perov");
+        Student student2 = new Student("Nikolai","Zakharov");
+        System.out.println(student1);
+        System.out.println(student2);
     }
 
 }
