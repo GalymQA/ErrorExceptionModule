77a98c0eea1165ef72c1122d29b468beab5508e4
GalymQA <galymakishevkz@gmail.com>
2022-04-20 17:11:14 +0600

    Added University Has No Department Exception

---
+++ src/main/java/exceptions/UniversityHasNoDepartmentException.java
@@ -0,0 +1,7 @@
+package main.java.exceptions;
+
+public class UniversityHasNoDepartmentException extends Exception {
+    public UniversityHasNoDepartmentException(String str) {
+        super(str);
+    }
+}
