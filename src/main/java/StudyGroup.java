380dffeff608668dcc9bbfaa6e3e8685109ae40b
GalymQA <galymakishevkz@gmail.com>
2022-04-20 15:31:46 +0600

    Added functions to StudyGroup class

--- src/main/java/StudyGroup.java
+++ src/main/java/StudyGroup.java
@@ -1,7 +1,62 @@
 package main.java;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+
 public class StudyGroup {
-
     private String studyGroupName;
+    private HashSet<Student> studentsOfStudyGroup;
 
+    public StudyGroup(String studyGroupName) {
+        this.studyGroupName = studyGroupName;
+    }
+
+    public String getStudyGroupName() {
+        return studyGroupName;
+    }
+
+    public HashSet<Student> getStudentsOfStudyGroup() {
+        return studentsOfStudyGroup;
+    }
+
+    public void setStudyGroupName(String studyGroupName) {
+        this.studyGroupName = studyGroupName;
+    }
+
+    public void setStudentsOfStudyGroup(HashSet<Student> studentsOfStudyGroup) {
+        this.studentsOfStudyGroup = studentsOfStudyGroup;
+    }
+
+    public void addStudentToStudyGroup(Student student) {
+        studentsOfStudyGroup.add(student);
+    }
+
+    public void addListOfStudentsToStudyGroup(List<Student> listOfStudents) {
+        studentsOfStudyGroup.addAll(listOfStudents);
+    }
+
+    @Override
+    public String toString() {
+        return "StudyGroup{" +
+                "studyGroupName='" + studyGroupName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        StudyGroup that = (StudyGroup) o;
+        return Objects.equals(studyGroupName, that.studyGroupName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studyGroupName);
+    }
 }
